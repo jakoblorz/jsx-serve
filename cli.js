e6320581cf247acb0f0904991375eb529100bad0
jakoblorz <jakob@lorz.org>
2017-12-09 16:17:01 +0100

    removed babel to keep dependencies slim, having no deps at this point

--- cli.js
+++ cli.js
@@ -5,7 +5,7 @@ const _viewFolder = path.join(_projectRoot, process.argv[2] || "./views");
 const _port = parseInt(process.argv[3] || process.env.PORT || "8080");
 const _host = process.argv[4] || process.env.HOSTNAME || "localhost";
 
-import { importViewDir, createServer } from "./index";
+const _lib = require("./index");
 
-const views = importViewDir(_viewFolder);
-const server = createServer(views).listen(_port, _host);
+const views = _lib.importViewDir(_viewFolder);
+const server = _lib.createServer(views).listen(_port, _host);
