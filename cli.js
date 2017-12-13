87fbba5464ad595865f23b33508aa2514e772b3e
jakoblorz <jakob@lorz.org>
2017-12-13 21:24:16 +0100

    created quick-and-dirty solution to reroute traffic

--- cli.js
+++ cli.js
@@ -4,10 +4,26 @@ const path = require("path");
 
 const _projectRoot = process.cwd() || __dirname;
 const _viewFolder = path.join(_projectRoot, process.argv[2] || "./views");
+/*
 const _port = parseInt(process.argv[3] || process.env.PORT || "8080");
 const _host = process.argv[4] || process.env.HOSTNAME || "localhost";
+*/
 
 const _lib = require("./index");
 
-const views = _lib.importViewDir(_viewFolder);
-const server = _lib.createServer(views).listen(_port, _host);
+let configuration = { defaults: { mode: "non-strict", port: 8080, host: "127.0.0.1" }, handlers: [] };
+
+try {
+    const rawConfiguration = _lib.importYamlConfigurationFromFile(path.join(
+        _viewFolder, process.argv[3] || "/.serveconf.yaml"));
+
+    configuration = _lib.parseConfigurationFromObject(rawConfiguration);
+
+    console.log(configuration);
+
+} catch (e) {
+    console.error(e);
+}
+
+const views = _lib.importViewDir(_viewFolder, configuration);
+const server = _lib.createServer(views).listen(configuration.defaults.port, configuration.defaults.host);
